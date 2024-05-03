c5afd41e6968316a3e2618b36e62d326fa8430cd
mo-joshuacolclough <joshua.colclough@metoffice.gov.uk>
2024-05-03 12:45:53 +0100

    Add stencil, restructure as test.

--- src/main.rs
+++ src/main.rs
@@ -60,21 +60,88 @@ fn linear_weight(s: &mut State<5>) {
 fn ad_linear_weight(s: &mut State<5>) {
     s["x"] = s["x"] + s["A"]*s["r"];
     s["y"] = s["y"] + s["B"]*s["r"];
+    s["r"] = 0.0;
 }
+
+// ---- STENCILS
+
+// Goes around the loop, smoothing. f[i] = f[i - 1] + f[i + 1] / 2.
+fn smooth(left: f32, right: f32) -> f32 {
+    left * 0.5 + right * 0.5
+}
+
+// Stencil size of 1.
+fn smooth_stencil<const N: usize>(f: &mut State<N>) {
+    // Edge case
+    f[0] = smooth(f[N - 1], f[1]);
+    for i in 1..=N-2 {
+        f[i] = smooth(f[i - 1], f[i + 1]);
+    }
+    f[N - 1] = smooth(f[N - 2], f[0]);
+}
+
+// NOTE: Same as linear weighting adjoint.
+fn ad_smooth(mut left: f32, mut right: f32, mut cent: f32) -> (f32, f32 ,f32) {
+    left = left + 0.5 * cent;
+    right = right + 0.5 * cent;
+    cent = 0.0;
+    (left, right, cent)
+}
+
+fn ad_smooth_stencil<const N: usize>(f: &mut State<N>) {
+    (f[N - 1], f[0], f[N - 1]) = ad_smooth(f[N - 2], f[0], f[N - 1]);
+    for i in N-2..=1 {
+        (f[i - 1], f[i + 1], f[i]) = ad_smooth(f[i - 1], f[i + 1], f[i]);
+    }
+    (f[N - 1], f[1], f[0]) = ad_smooth(f[N - 1], f[1], f[0]);
+}
+
+// -----------------
 
 // ================================================================================================
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        // ACTIVE                                   T    T    T
+        test_adjoint("+", add, ad_add, State::new([2.0, 3.0, 0.0],
+                                             Some(["a", "b", "r"])));
+    }
+
+    #[test]
+    fn test_axpy() {
+        // ACTIVE                                        F    T    T
+        test_adjoint("axpy", axpy, ad_axpy, State::new([1.0, 2.0, 5.0],
+                                                  Some(["a", "x", "y"])));
+    }
+
+    #[test]
+    fn test_linear_weight() {
+        test_adjoint("linear_weight", linear_weight, ad_linear_weight,
+        // ACTIVE                 T    F    T    F    T
+                     State::new([0.0, 0.8, 2.3, 0.2, 3.2],
+                           Some(["r", "A", "x", "B", "y"])));
+    }
+     
+    #[test]
+    fn test_stencil() {
+        let stencil = (1.0, 2.0, 3.0);
+        let ad_stencil_result = ad_smooth(stencil.0, stencil.1, stencil.2);
+        
+        let mut stencil_state = State::new([stencil.2, 0.5, stencil.0, 0.5, stencil.1],
+                                  Some(["r", "A", "x", "B", "y"]));
+        ad_linear_weight(&mut stencil_state);
+
+        println!("AD_SMOOTH: {:?}, AD_LINEAR {:?}", ad_stencil_result, stencil_state);
+
+        test_adjoint("smooth_stencil", smooth_stencil, ad_smooth_stencil,
+                     State::new([0.2, 0.5, 0.6, 1.3], None));
+    }
+
+}
+
 fn main() {
-    // ACTIVE                                   T    T    T
-    test_adjoint("+", add, ad_add, State::new([2.0, 3.0, 0.0],
-                                              ["a", "b", "r"]));
-
-    // ACTIVE                                        F    T    T
-    test_adjoint("axpy", axpy, ad_axpy, State::new([1.0, 2.0, 5.0],
-                                                   ["a", "x", "y"]));
-
-    test_adjoint("linear_weight", linear_weight, ad_linear_weight,
-    // ACTIVE                 T    F    T    F    T
-                 State::new([0.0, 0.8, 2.3, 0.2, 3.2],
-                            ["r", "A", "x", "B", "y"]));
 }
--- src/state.rs
+++ src/state.rs
@@ -6,22 +6,24 @@ use std::ops::{Index, IndexMut};
 #[derive(Clone, Debug)]
 pub struct State<const N: usize> {
     x: SVector<f32, N>,
-    names: HashMap<&'static str, usize>,   // Index mapping.
+    names: HashMap<String, usize>,   // Index mapping.
 }
 
 impl<const N: usize> State<N> {
-    pub fn new(vals: impl Into<SVector<f32, N>>, names_st: [&'static str; N]) -> Self {
+    pub fn new(vals: impl Into<SVector<f32, N>>, names_st_opt: Option<[&'static str; N]>) -> Self {
         let mut map = HashMap::new();
 
-        for (idx, name) in names_st.iter().enumerate() {
-            map.insert(*name, idx);
-        }
+        if let Some(names_st) = names_st_opt {
+            for (idx, name) in names_st.iter().enumerate() {
+                map.insert(String::from(*name), idx);
+            }
+        } else {
+            for i in 0..N {
+                map.insert(i.to_string(), i);
+            }
+        };
 
         Self { x: vals.into(), names: map }
-    }
-
-    pub fn vec_ref_mut(&mut self) -> &mut SVector<f32, N> {
-        &mut self.x
     }
 
     pub fn vec_ref(&self) -> &SVector<f32, N> {
@@ -44,4 +46,14 @@ impl<const N: usize> IndexMut<&'static str> for State<N> {
         &mut self.x[self.names[name]]
     }
 }
+
+impl<const N: usize> Index<usize> for State<N> {
+    type Output = f32;
+    fn index(&self, idx: usize) -> &Self::Output { return self.x.index(idx); }
+}
+
+impl<const N: usize> IndexMut<usize> for State<N> {
+    fn index_mut(&mut self, idx: usize) -> &mut Self::Output { return self.x.index_mut(idx); }
+}
+
 
