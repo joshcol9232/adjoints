6197f87ea68692f4c0974a33b1b2331c2853fcf2
mo-joshuacolclough <joshua.colclough@metoffice.gov.uk>
2024-05-03 11:07:31 +0100

    Linear weight example

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,14 @@ fn test_adjoint<const N: usize>(func_name: &'static str,
 
     let abs_diff = (mdx_dot - mtranspose_m_dx_dot).abs();
     let relative_to_ep = (abs_diff / f32::EPSILON) as usize;
-    println!("'{}' => {:?}", func_name, relative_to_ep);
+    println!("'{}' => {:?} {:?} : {:?}", func_name, mdx_dot, mtranspose_m_dx_dot, relative_to_ep);
         
     assert!(relative_to_ep <= TOLERANCE);
 }
 
 // ================================================================================================
 
+// a(in), b(in), r(inout)
 fn add(s: &mut State<3>) {
     s["r"] = s["a"] + s["b"];
 }
@@ -40,14 +41,25 @@ fn ad_add(s: &mut State<3>) {
     s["r"] = 0.0;
 }
 
+// x(in), a(in), y(inout)
 fn axpy(s: &mut State<3>) {
     s["y"] = s["a"] * s["x"] + s["y"];
 }
 
 fn ad_axpy(s: &mut State<3>) {
     // x = x + a * y;
-    s["x"] = s["x"] + s["a"] + s["y"];
+    s["x"] = s["x"] + s["a"] * s["y"];
     // y = y;
+}
+
+// r(inout) = A(in) x(in) + B(in) y(in)
+fn linear_weight(s: &mut State<5>) {
+    s["r"] = s["A"] * s["x"] + s["B"] * s["y"];
+}
+
+fn ad_linear_weight(s: &mut State<5>) {
+    s["x"] = s["x"] + s["A"]*s["r"];
+    s["y"] = s["y"] + s["B"]*s["r"];
 }
 
 // ================================================================================================
@@ -60,4 +72,9 @@ fn main() {
     // ACTIVE                                        F    T    T
     test_adjoint("axpy", axpy, ad_axpy, State::new([1.0, 2.0, 5.0],
                                                    ["a", "x", "y"]));
+
+    test_adjoint("linear_weight", linear_weight, ad_linear_weight,
+    // ACTIVE                 T    F    T    F    T
+                 State::new([0.0, 0.8, 2.3, 0.2, 3.2],
+                            ["r", "A", "x", "B", "y"]));
 }
